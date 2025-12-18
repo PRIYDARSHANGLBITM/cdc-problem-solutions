a4389f97accd20a40d5868c660f898869be92811
PRIY DARSHAN <priydarshan12345@gmail.com>
2025-12-18 16:17:44 +0530

    Add reverse array after M solution

---
+++ Reversingarray.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Reversingarray {
+
+    // Function to reverse array after position m
+    public static void reverseArrayAfterM(int[] arr, int m) {
+        int start = m + 1;
+        int end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        // Sample input (you can modify this)
+        int[][] testArrays = {
+            {1, 2, 3, 4, 5, 6},
+            {10, 9, 8, 7, 6}
+        };
+        int[] positions = {3, 2}; // M values for each test case
+
+        for (int t = 0; t < testArrays.length; t++) {
+            int[] arr = testArrays[t];
+            int m = positions[t];
+            reverseArrayAfterM(arr, m);
+
+            // Print the output
+            for (int num : arr) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
