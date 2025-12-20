ff8f676232f56ca81e07cc47b70706325e8f30c3
PRIY DARSHAN <priydarshan12345@gmail.com>
2025-12-20 19:34:55 +0530

    Added Max and Min element in array

---
+++ MaxMinArray.java
@@ -0,0 +1,17 @@
+public class MaxMinArray {
+    public static void main(String[] args) {
+        int[] arr = {3, 5,1,8,2,9,7};
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+            if(arr[i]<min){
+                min = arr[i];
+            }
+            }
+            System.out.println("Maximum value: " + max);
+            System.out.println("Minimum value: " + min);
+        }
+    }
